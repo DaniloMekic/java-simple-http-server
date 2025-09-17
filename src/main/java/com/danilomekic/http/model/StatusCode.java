4854f123cea35dcc37a702fdcf8e6257b60d8b9d
DaniloMekic <danilo.mekic@gmail.com>
2025-09-17 15:22:56 +0200

    Add enum class as a model for status codes.

---
+++ src/main/java/com/danilomekic/http/model/StatusCode.java
@@ -0,0 +1,60 @@
+package com.danilomekic.http.model;
+
+/*
+* RFC 9110: Section 15
+*/
+public enum StatusCode {
+    // 1XX Informational
+    CONTINUE(100, "Continue"),
+    SWITCHING_PROTOCOLS(101, "Switching Protocols"),
+    EARLY_HINTS(103, "Early Hints"),
+
+    // 2XX Success
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    ACCEPTED(202, "Accepted"),
+    NO_CONTENT(2024, "No Content"),
+
+    // 3XX Redirection
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    NOT_MODIFIED(304, "Not Modified"),
+
+    // 4XX Client Error
+    BAD_REQUEST(400, "Bad Requestq"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+
+    // 5XX Server Error
+    INTERNAT_SERVER_ERROR(500, "Internal Server Error"),
+    NOT_IMPLEMENTED(501, "Not Implemented"),
+    BAD_GATEWAY(502, "Bad Gateway"),
+    SERVICE_UNAVAILABLE(503, "Service Unavailable");
+
+    private final int statusCode;
+    private final String reasonPhrase;
+
+    private StatusCode(int statusCode, String reasonPhrase) {
+        this.statusCode = statusCode;
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getReasonPhrase() {
+        return reasonPhrase;
+    }
+
+    public static StatusCode fromCode(int code) {
+        for (StatusCode status : values()) {
+            if (status.statusCode == code) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unknown HTTP Status Code: " + code);
+    }
+}
