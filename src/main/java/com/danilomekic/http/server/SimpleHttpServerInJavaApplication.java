3f88264d86a640dcb0d606a41bdea900e7708e1b
DaniloMekic <danilo.mekic@gmail.com>
2025-09-17 13:34:56 +0200

    Project init.

---
+++ src/main/java/com/danilomekic/http/server/SimpleHttpServerInJavaApplication.java
@@ -0,0 +1,12 @@
+package com.danilomekic.http.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHttpServerInJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleHttpServerInJavaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/danilomekic/java_simple_http_server/SimpleHttpServerInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilomekic.java_simple_http_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHttpServerInJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
