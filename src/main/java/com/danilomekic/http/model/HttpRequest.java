6e07e073e7fa20e16fcac939290e60c5278f896d
DaniloMekic <danilo.mekic@gmail.com>
2025-09-19 08:22:24 +0200

    Expand request-line array field in a model for request message to individual fields.

--- src/main/java/com/danilomekic/http/model/HttpRequest.java
+++ src/main/java/com/danilomekic/http/model/HttpRequest.java
@@ -3,4 +3,4 @@ package com.danilomekic.http.model;
 import java.util.List;
 import java.util.Map;
 
-public record HttpRequest(String[] requestLine, Map<String, List<String>> requestHeaders, byte[] requestBody) {}
+public record HttpRequest(String requestMethod, String requestTarget, String httpVersion, Map<String, List<String>> requestHeaders, byte[] requestBody) {}
