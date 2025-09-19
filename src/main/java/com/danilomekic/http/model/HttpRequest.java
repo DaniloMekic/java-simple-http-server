dbb9e40803c240437416988855ce052140456609
DaniloMekic <danilo.mekic@gmail.com>
2025-09-19 12:26:18 +0200

    Add comment to HTTP request model class regarding RFC formatting.

--- src/main/java/com/danilomekic/http/model/HttpRequest.java
+++ src/main/java/com/danilomekic/http/model/HttpRequest.java
@@ -3,4 +3,14 @@ package com.danilomekic.http.model;
 import java.util.List;
 import java.util.Map;
 
+ /*
+ * RFC 9112
+ * HTTP Request Message Format = Request-line<CR><LF>
+ *                               *[Header-line]<CR><LF>
+ *                               <CR><LF>
+ *                               Body
+ *
+ * <CR> = \u000D | 13 | 0x0D | 0o15 | 0b00001101
+ * <LF> = \u000A | 10 | 0x0A | 0o12 | 0b00001010
+*/
 public record HttpRequest(String requestMethod, String requestTarget, String httpVersion, Map<String, List<String>> requestHeaders, byte[] requestBody) {}
