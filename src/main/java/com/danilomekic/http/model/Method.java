855f9d85ebf15c6c255baf8fd70f99adbebe2917
DaniloMekic <danilo.mekic@gmail.com>
2025-09-19 15:41:22 +0200

    Add enum class for request method.

---
+++ src/main/java/com/danilomekic/http/model/Method.java
@@ -0,0 +1,26 @@
+package com.danilomekic.http.model;
+
+/*
+* RFC 9110: Section 9
+*/
+public enum Method {
+    GET(true),
+    HEAD(true),
+    POST(false),
+    PUT(false),
+    DELETE(false),
+    CONNECT(false),
+    OPTIONS(true),
+    TRACE(true),
+    PATCH(false);
+
+    private final boolean isSafe;
+
+    private Method(boolean isSafe) {
+        this.isSafe = isSafe;
+    }
+
+    public boolean isSafe() {
+        return isSafe;
+    }
+}
