e840c5e7144a1d76b1d398a5dd00bf34a56681e5
DaniloMekic <danilo.mekic@gmail.com>
2025-09-23 15:28:21 +0200

    Remove serialization of HTTP response. Instead, use PrintWriter.

--- src/main/java/com/danilomekic/http/serializer/HttpResponseSerializer.java
+++
@@ -1,9 +0,0 @@
-package com.danilomekic.http.serializer;
-
-import java.io.IOException;
-
-import com.danilomekic.http.model.HttpResponse;
-
-public interface HttpResponseSerializer {
-    byte[] serialize(HttpResponse httpResponse) throws IOException;
-}
