d6c850b4286c8d246a001e2a9a6c60db38b8a866
DaniloMekic <danilo.mekic@gmail.com>
2025-09-23 15:35:14 +0200

    Rename variable names for stream objects to better reflect their semantics.

--- src/main/java/com/danilomekic/http/server/HttpConnectionHandler.java
+++ src/main/java/com/danilomekic/http/server/HttpConnectionHandler.java
@@ -11,7 +11,6 @@ import com.danilomekic.http.model.HttpRequest;
 import com.danilomekic.http.model.HttpResponse;
 import com.danilomekic.http.parser.HttpRequestParser;
 import com.danilomekic.http.parser.SimpleHttpRequestParser;
-import com.danilomekic.http.serializer.SimpleHttpResponseSerializer;
 import com.danilomekic.http.writer.HttpResponseWriter;
 import com.danilomekic.http.writer.SimpleHttpResponseWriter;
 
@@ -19,21 +18,21 @@ public class HttpConnectionHandler implements ConnectionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpConnectionHandler.class);
 
     private final HttpRequestParser httpRequestParser = new SimpleHttpRequestParser();
-    private final HttpResponseWriter httpResponseWriter = new SimpleHttpResponseWriter(new SimpleHttpResponseSerializer());
+    private final HttpResponseWriter httpResponseWriter = new SimpleHttpResponseWriter();
 
     @Override
     public void handle(Socket socket) {
         try (
-            InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream();
+            InputStream socketInputStream = socket.getInputStream();
+            OutputStream socketOutputStream = socket.getOutputStream();
             ) {
             LOGGER.info("Parsing HTTP request");
 
-            HttpRequest httpRequest = httpRequestParser.parse(inputStream);
+            HttpRequest httpRequest = httpRequestParser.parse(socketInputStream);
 
             HttpResponse httpResponse = new HttpResponse(200, "OK", "plain/text", null);
 
-            httpResponseWriter.write(httpResponse, outputStream);
+            httpResponseWriter.write(httpResponse, socketOutputStream);
         } catch (Exception e) {
             LOGGER.error("Error handling connection: {}", e);
         }
