18b5adee89422bf7ccfbf5556ddfd65ec9fe6efa
DaniloMekic <danilo.mekic@gmail.com>
2025-09-25 15:45:41 +0200

    Add URISyntaxException to request parser, since HttpRequest record class now uses URI object to represente request target.

--- src/main/java/com/danilomekic/http/server/parser/HttpRequestParser.java
+++ src/main/java/com/danilomekic/http/server/parser/HttpRequestParser.java
@@ -2,9 +2,10 @@ package com.danilomekic.http.server.parser;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 
 import com.danilomekic.http.server.model.HttpRequest;
 
 public interface HttpRequestParser {
-    HttpRequest parse(InputStream inputStream) throws IOException;
+    HttpRequest parse(InputStream inputStream) throws IOException, URISyntaxException;
 }
