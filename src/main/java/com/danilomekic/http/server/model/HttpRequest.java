7d5ce95aee7a5ec6318a6e857c1e5dedae09fbf2
DaniloMekic <danilo.mekic@gmail.com>
2025-09-25 15:43:18 +0200

    Update HttpRequest record class to use URI object to represent request target.

--- src/main/java/com/danilomekic/http/server/model/HttpRequest.java
+++ src/main/java/com/danilomekic/http/server/model/HttpRequest.java
@@ -1,5 +1,6 @@
 package com.danilomekic.http.server.model;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
@@ -13,4 +14,4 @@ import java.util.Map;
  * <CR> = \u000D | 13 | 0x0D | 0o15 | 0b00001101
  * <LF> = \u000A | 10 | 0x0A | 0o12 | 0b00001010
 */
-public record HttpRequest(String requestMethod, String requestTarget, String httpVersion, Map<String, List<String>> requestHeaders, byte[] requestBody) {}
+public record HttpRequest(String requestMethod, URI requestTarget, String httpVersion, Map<String, List<String>> requestHeaders, byte[] requestBody) {}
