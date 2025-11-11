453a272713c770d2e5884b21297b89ac30f58a93
DaniloMekic <danilo.mekic@gmail.com>
2025-11-11 17:43:00 +0100

    Extend Route interface so it better handles 404 and 405 errors.

--- src/main/java/com/danilomekic/http/server/router/Route.java
+++ src/main/java/com/danilomekic/http/server/router/Route.java
@@ -2,8 +2,16 @@ package com.danilomekic.http.server.router;
 
 import com.danilomekic.http.server.model.HttpRequest;
 import com.danilomekic.http.server.model.HttpResponse;
+import com.danilomekic.http.server.model.Method;
+
+import java.util.Set;
 
 public interface Route {
     boolean matches(HttpRequest httpRequest);
+
     HttpResponse handle(HttpRequest httpRequest);
+
+    boolean matchesPath(HttpRequest httpRequest);
+
+    Set<Method> getSupportedMethods();
 }
